09da6ed5c9e753e252b2be5f8dd85792428a0e6e
Wong Golden <thiep.wong@gmail.com>
2021-12-10 15:33:49 +0700

    Cap nhat

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<sunmi_flutter_print/SunmiFlutterPrintPlugin.h>)
+#import <sunmi_flutter_print/SunmiFlutterPrintPlugin.h>
+#else
+@import sunmi_flutter_print;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SunmiFlutterPrintPlugin registerWithRegistrar:[registry registrarForPlugin:@"SunmiFlutterPrintPlugin"]];
+}
+
+@end
